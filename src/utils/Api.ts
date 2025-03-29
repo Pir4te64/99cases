4d8a885fedc9f6ed6b88171d39ad2328e635d4e8
Pir4te64 <vicmor601@gmail.com>
2025-03-29 17:05:21 -0300

    Agrega nuevas rutas API para la gestión de casos, modelos, colores, materiales y órdenes, mejorando la funcionalidad del sistema.

--- src/utils/Api.ts
+++ src/utils/Api.ts
@@ -1,5 +1,16 @@
 export const baseURL = "https://api-cases99.nourish.com.ar/v1/";
+
 export const API = {
   login: `${baseURL}/login`,
   register: `${baseURL}/register`,
+  createCase: `${baseURL}/cases/create`,
+  updateCaseImage: `${baseURL}/cases/upload-image`,
+  models: `${baseURL}/phone-models/create`,
+  modelsGetAll: `${baseURL}/phone-models/get-all`,
+  colors: `${baseURL}/cases/colors/create`,
+  colorsGetAll: `${baseURL}/cases/colors/get-all`,
+  materials: `${baseURL}/materials/create`,
+  materialsGetAll: `${baseURL}/materials/get-all`,
+  order: `${baseURL}/orders/create`,
+  orderGetAll: `${baseURL}/orders/get-all`,
 };
